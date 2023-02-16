2c8d18d097976750458ec11131556a6efda1ded0
rmorshea <ryan.morshead@gmail.com>
2023-02-16 09:31:53 -0800

    add test infra + misc fixes

---
+++ .github/pull_request_template.md
@@ -0,0 +1,6 @@
+## Checklist
+
+Please update this checklist as you complete each item:
+
+- [ ] Tests have been included for all bug fixes or added functionality.
+- [ ] GitHub Issues which may be closed by this Pull Request have been linked.
--- tests/test_noxopt.py
+++ tests/test_noxopt.py
@@ -1,16 +1,17 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING, Sequence
-from nox._decorators import Func
-from nox import Session
-from nox._options import options as nox_options
-from nox.manifest import Manifest
-from noxopt import NoxOpt, Option
 
 import pytest
+from nox._decorators import Func
+from nox._options import options as nox_options
+from nox.manifest import Manifest
+
+from noxopt import Annotated, NoxOpt, Option, Session
+
 
 if TYPE_CHECKING:
-    from typing import Callable, Protocol
+    from typing import Protocol
 
     class Executor(Protocol):
         def __call__(
@@ -23,15 +24,19 @@ if TYPE_CHECKING:
 
 
 @pytest.fixture
-def execute(monkeypatch: pytest.MonkeyPatch) -> Executor:
+def registry(monkeypatch) -> dict[str, Func]:
     funcs: dict[str, Func] = {}
     monkeypatch.setattr("nox.registry._REGISTRY", funcs)
+    return funcs
 
+
+@pytest.fixture
+def execute(registry) -> Executor:
     def execute(session: str = "", tag: str = "", posargs: Sequence[str] = ()) -> None:
-        manifest = Manifest(funcs, nox_options.namespace(posargs=posargs))
+        manifest = Manifest(registry, nox_options.namespace(posargs=posargs))
 
         to_exec = [session] if session else []
-        to_exec.extend(k for k, v in funcs.items() if tag in v.tags)
+        to_exec.extend(k for k, v in registry.items() if tag in v.tags)
         for func_name in to_exec:
             try:
                 manifest[func_name].execute()
@@ -41,15 +46,182 @@ def execute(monkeypatch: pytest.MonkeyPatch) -> Executor:
     return execute
 
 
-def test_add_session(execute: Executor):
+def test_session_with_simple_option(execute: Executor):
     app = NoxOpt()
 
-    some_option_value = None
+    number_value = None
+
+    @app.session(venv_backend="none")
+    def my_session(session: Session, number: int = 0) -> None:
+        nonlocal number_value
+        number_value = number
+
+    execute(session="my-session")
+    assert number_value == 0
+    execute(session="my-session", posargs=["--number", "1"])
+    assert number_value == 1
+
+
+def test_session_with_annotated_option(execute: Executor):
+    app = NoxOpt()
+
+    number_value = None
+
+    @app.session(venv_backend="none")
+    def my_session(
+        session: Session,
+        # double the given number
+        number: Annotated[int, Option(type=lambda i: int(i) * 2)] = 0,
+    ) -> None:
+        nonlocal number_value
+        number_value = number
+
+    execute(session="my-session", posargs=["--number", "2"])
+    assert number_value == 4
+
+
+def test_all_options_are_flags(execute: Executor):
+    app = NoxOpt()
+
+    with pytest.raises(ValueError, match="only supports flags"):
+
+        @app.session(venv_backend="none")
+        def my_session(
+            session: Session, number: Annotated[int, Option(flags=["not-a-flag"])] = 0
+        ) -> None:
+            ...
+
+
+def test_expect_metadata_to_be_option(execute: Executor):
+    app = NoxOpt()
+
+    with pytest.raises(ValueError, match="metadata must be an Option"):
+
+        @app.session(venv_backend="none")
+        def my_session(
+            session: Session, number: Annotated[int, "not an option"] = 0
+        ) -> None:
+            ...
+
+
+def test_only_kwarg_params(execute: Executor):
+    app = NoxOpt()
+
+    with pytest.raises(TypeError, match="Found non-keyword session parameters"):
+
+        @app.session(venv_backend="none")
+        def my_session(session: Session, *args: int) -> None:
+            ...
+
+    with pytest.raises(TypeError, match="Found non-keyword session parameters"):
+
+        @app.session(venv_backend="none")
+        def my_session(session: Session, **args: int) -> None:
+            ...
+
+
+def test_no_extra_metadata(execute: Executor):
+    app = NoxOpt()
+
+    with pytest.raises(ValueError, match="has extra metadata"):
+
+        @app.session(venv_backend="none")
+        def my_session(
+            session: Session, number: Annotated[int, Option(), "extra-junk"] = 0
+        ) -> None:
+            ...
+
+
+def tests_error_on_conflicting_options(execute: Executor):
+    app = NoxOpt()
 
     @app.session(venv_backend="none")
     def my_session(session: Session, some_option: int = 0) -> None:
-        nonlocal some_option_value
-        some_option_value = some_option
+        ...
 
-    execute(session="my-session", posargs=["--some-option", "0"])
-    assert some_option_value == 0
+    with pytest.raises(ValueError, match="Conflicting session options"):
+
+        @app.session(venv_backend="none")
+        def my_session(session: Session, some_option: bool = 0) -> None:
+            ...
+
+    @app.session(venv_backend="none")
+    def my_session(
+        session: Session,
+        some_option: Annotated[int, Option(type=int)] = 0,
+    ) -> None:
+        ...
+
+    with pytest.raises(ValueError, match="Conflicting session options"):
+
+        @app.session(venv_backend="none")
+        def my_session(
+            session: Session,
+            some_option: Annotated[int, Option(type=int, help="different")] = 0,
+        ) -> None:
+            ...
+
+
+def test_auto_tags_no_prefix(registry):
+    app = NoxOpt(auto_tag_depth=2)
+
+    @app.session
+    def check_python_tests(session: Session) -> None:
+        ...
+
+    @app.session
+    def check_python_format(session: Session) -> None:
+        ...
+
+    @app.session
+    def check_javascript_tests(session: Session) -> None:
+        ...
+
+    @app.session
+    def check_javascript_format(session: Session) -> None:
+        ...
+
+    assert set(registry["check-python-tests"].tags) == {"check-python", "check"}
+    assert set(registry["check-python-format"].tags) == {"check-python", "check"}
+    assert set(registry["check-javascript-tests"].tags) == {"check-javascript", "check"}
+    assert set(registry["check-javascript-format"].tags) == {
+        "check-javascript",
+        "check",
+    }
+
+
+def test_auto_tags_with_prefix(registry):
+    app = NoxOpt(auto_tag_depth=2, prefix="session-prefix")
+
+    @app.session
+    def check_python_tests(session: Session) -> None:
+        ...
+
+    @app.session
+    def check_python_format(session: Session) -> None:
+        ...
+
+    @app.session
+    def check_javascript_tests(session: Session) -> None:
+        ...
+
+    @app.session
+    def check_javascript_format(session: Session) -> None:
+        ...
+
+    assert set(registry["session-prefix-check-python-tests"].tags) == {
+        "session-prefix-check-python",
+        "session-prefix-check",
+    }
+    assert set(registry["session-prefix-check-python-format"].tags) == {
+        "session-prefix-check-python",
+        "session-prefix-check",
+    }
+    assert set(registry["session-prefix-check-javascript-tests"].tags) == {
+        "session-prefix-check-javascript",
+        "session-prefix-check",
+    }
+    assert set(registry["session-prefix-check-javascript-format"].tags) == {
+        "session-prefix-check-javascript",
+        "session-prefix-check",
+    }
