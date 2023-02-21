9c77690389f397c4afa181da925f7aa1ea6404a4
Ryan Morshead <ryan.morshead@gmail.com>
2023-02-20 19:48:47 -0800

    Fix bool (#5)
    * fix bool handling + organize code
    
    * misc cleanup
    
    * test required param
    
    * minor fixes

---
+++ noxopt/_tagging.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    # avoid import error if this private module ever changes in the future
+    from nox._decorators import Func
+
+
+class AutoTag:
+    r"""Session auto tagging utility
+
+    Construct a graph of words, and at every branching point in the graph it creates a
+    tag. For example the session names:
+
+    - `a-x-1`
+    - `a-x-2`
+    - `a-y-1`
+    - `a-y-2`
+    - `b-x-1`
+    - `b-x-2`
+
+    Would create the graph:
+
+    ```
+          *
+         / \
+        a   b
+       /\    \
+      x  y    x
+     /|  |\   |\
+    1 2  1 2  1 2
+    ```
+
+    At every branching point in the graph a tag would be generated and applied to all
+    functions with that prefix. So the tags would be:
+
+    - `a`
+    - `a-x`
+    - `a-y`
+    - `b-x`
+
+    This isn't always perfect since technically, we could subtitute the tag `b-x` for
+    just `b` since the shorted common string between `b-x-1` and `b-x-2` is `b`. But
+    it works most of the time, and it makes the algorithm a bit easier to reason about.
+    """
+
+    def __init__(self, sep: str = "-"):
+        self._sep = sep
+        self._tag_tree = _TagNode()
+
+    def add_func(self, name: str, func: Func) -> None:
+        node = self._tag_tree
+
+        # each branching point in the graph represents a tag
+        for word in name.split(self._sep):
+            if word in node.children:
+                # this is a branching point in the graph
+                if len(node.children) > 1:
+                    node.add_tag(func)
+                node = node.children[word]
+            else:
+                if len(node.children) == 1:
+                    node.add_tag(func)
+                    # We're about to create a new branching point - funcs added earlier
+                    # will not have this nodes tag.
+                    node.retroactively_add_tags()
+                node = node.add_node(word, self._sep)
+
+        # add this func to the last node
+        node.add_func(func)
+
+
+class _TagNode:
+    def __init__(self, parent: _TagNode | None = None, tag: str | None = None):
+        self.parent = parent
+        self.tag = tag
+        self.funcs: list[Func] = []
+        self.children: dict[str, _TagNode] = {}
+
+    def add_node(self, word: str, sep: str) -> _TagNode:
+        tag = f"{self.tag}{sep}{word}" if self.tag else word
+        child = self.children[word] = _TagNode(self, tag)
+        return child
+
+    def add_func(self, func: Func) -> None:
+        self.funcs.append(func)
+
+    def add_tag(self, func: Func) -> None:
+        if self.tag:
+            func.tags.append(self.tag)
+
+    def retroactively_add_tags(self) -> None:
+        to_visit = [self]
+        while to_visit:
+            node = to_visit.pop()
+            for f in node.funcs:
+                self.add_tag(f)
+            to_visit.extend(node.children.values())
--- tests/test_noxopt.py
+++ tests/test_noxopt.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+from contextlib import ExitStack
 from typing import TYPE_CHECKING, Sequence
 
 import pytest
@@ -19,39 +21,85 @@ if TYPE_CHECKING:
             session: str = "",
             tag: str = "",
             posargs: Sequence[str] = ...,
+            raises: Exception | tuple[type[Exception], str | re.Pattern] | None = ...,
+            prints: str | re.Pattern | None = ...,
+            logs: str | re.Pattern | None = ...,
         ) -> None:
             ...
 
 
 @pytest.fixture
-def registry(monkeypatch) -> dict[str, Func]:
+def registry(monkeypatch: pytest.MonkeyPatch) -> dict[str, Func]:
     funcs: dict[str, Func] = {}
     monkeypatch.setattr("nox.registry._REGISTRY", funcs)
     return funcs
 
 
 @pytest.fixture
-def execute(registry) -> Executor:
-    def execute(session: str = "", tag: str = "", posargs: Sequence[str] = ()) -> None:
+def execute(
+    registry: dict[str, Func],
+    capsys: pytest.CaptureFixture,
+    caplog: pytest.LogCaptureFixture,
+) -> Executor:
+    def execute(
+        session: str = "",
+        tag: str = "",
+        posargs: Sequence[str] = (),
+        raises: Exception | tuple[type[Exception], str | re.Pattern] | None = None,
+        prints: str | re.Pattern | None = None,
+        logs: str | re.Pattern | None = None,
+    ) -> None:
         manifest = Manifest(registry, nox_options.namespace(posargs=posargs))
+
+        if raises:
+            if isinstance(raises, tuple):
+                etype, epat = raises
+            else:
+                etype, epat = type(raises), str(raises)
+
+            if not isinstance(epat, re.Pattern):
+                epat = re.compile(epat)
+        else:
+            etype = epat = None
 
         to_exec = [session] if session else []
         to_exec.extend(k for k, v in registry.items() if tag in v.tags)
         for func_name in to_exec:
-            try:
+            with ExitStack() as context:
+                if etype and epat:
+                    context.enter_context(pytest.raises(etype, match=epat))
                 manifest[func_name].execute()
-            except SystemExit as error:
-                raise RuntimeError(*error.args) from error
+
+        if prints:
+            if not isinstance(prints, re.Pattern):
+                prints = re.compile(prints)
+
+            readout = capsys.readouterr()
+            for line in readout.out.split("\n") + readout.err.split("\n"):
+                if prints.search(line):
+                    break
+            else:
+                raise AssertionError(f"No output matches pattern {prints.pattern!r}")
+
+        if logs:
+            if not isinstance(logs, re.Pattern):
+                logs = re.compile(logs)
+
+            for line in caplog.messages:
+                if logs.search(line):
+                    break
+            else:
+                raise AssertionError(f"No log matches pattern {logs.pattern!r}")
 
     return execute
 
 
 def test_session_with_simple_option(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
     number_value = None
 
-    @app.session
+    @group.session
     def my_session(session: Session, number: int = 0) -> None:
         nonlocal number_value
         number_value = number
@@ -63,11 +111,11 @@ def test_session_with_simple_option(execute: Executor):
 
 
 def test_session_with_annotated_option(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
     number_value = None
 
-    @app.session
+    @group.session
     def my_session(
         session: Session,
         # double the given number
@@ -81,11 +129,11 @@ def test_session_with_annotated_option(execute: Executor):
 
 
 def test_all_options_are_flags(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
     with pytest.raises(ValueError, match="only supports flags"):
 
-        @app.session
+        @group.session
         def my_session(
             session: Session, number: Annotated[int, Option(flags=["not-a-flag"])] = 0
         ) -> None:
@@ -93,11 +141,11 @@ def test_all_options_are_flags(execute: Executor):
 
 
 def test_expect_metadata_to_be_option(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
     with pytest.raises(ValueError, match="metadata must be an Option"):
 
-        @app.session
+        @group.session
         def my_session(
             session: Session, number: Annotated[int, "not an option"] = 0
         ) -> None:
@@ -105,27 +153,27 @@ def test_expect_metadata_to_be_option(execute: Executor):
 
 
 def test_only_kwarg_params(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
     with pytest.raises(TypeError, match="Found non-keyword session parameters"):
 
-        @app.session
+        @group.session
         def my_session(session: Session, *args: int) -> None:
             ...
 
     with pytest.raises(TypeError, match="Found non-keyword session parameters"):
 
-        @app.session
+        @group.session
         def my_session(session: Session, **args: int) -> None:
             ...
 
 
 def test_no_extra_metadata(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
     with pytest.raises(ValueError, match="has extra metadata"):
 
-        @app.session
+        @group.session
         def my_session(
             session: Session, number: Annotated[int, Option(), "extra-junk"] = 0
         ) -> None:
@@ -133,19 +181,19 @@ def test_no_extra_metadata(execute: Executor):
 
 
 def tests_error_on_conflicting_options(execute: Executor):
-    app = NoxOpt()
+    group = NoxOpt()
 
-    @app.session
+    @group.session
     def my_session(session: Session, some_option: int = 0) -> None:
         ...
 
     with pytest.raises(ValueError, match="Conflicting session options"):
 
-        @app.session
+        @group.session
         def my_session(session: Session, some_option: bool = 0) -> None:
             ...
 
-    @app.session
+    @group.session
     def my_session(
         session: Session,
         some_option: Annotated[int, Option(type=int)] = 0,
@@ -154,7 +202,7 @@ def tests_error_on_conflicting_options(execute: Executor):
 
     with pytest.raises(ValueError, match="Conflicting session options"):
 
-        @app.session
+        @group.session
         def my_session(
             session: Session,
             some_option: Annotated[int, Option(type=int, help="different")] = 0,
@@ -163,29 +211,29 @@ def tests_error_on_conflicting_options(execute: Executor):
 
 
 def test_auto_tags_no_prefix(registry):
-    app = NoxOpt(auto_tag=True)
+    group = NoxOpt(auto_tag=True)
 
-    @app.session
+    @group.session
     def a_x_1(session: Session) -> None:
         ...
 
-    @app.session
+    @group.session
     def a_x_2(session: Session) -> None:
         ...
 
-    @app.session
+    @group.session
     def a_y_1(session: Session) -> None:
         ...
 
-    @app.session
+    @group.session
     def a_y_2(session: Session) -> None:
         ...
 
-    @app.session
+    @group.session
     def b_x_1(session: Session) -> None:
         ...
 
-    @app.session
+    @group.session
     def b_x_2(session: Session) -> None:
         ...
 
@@ -198,40 +246,40 @@ def test_auto_tags_no_prefix(registry):
 
 
 def test_setup_funcs(execute: Executor):
-    app = NoxOpt(auto_tag=True)
+    group = NoxOpt(auto_tag=True)
 
     calls = []
 
-    @app.setup
+    @group.setup
     def setup_app(session: Session):
         calls.append("setup-all")
 
-    @app.setup("session-x")
+    @group.setup("session-x")
     def setup_x(session: Session, setup_param: str = "not-set") -> None:
         assert setup_param == "is-set"
         calls.append("setup-x")
 
-    @app.setup("session-y")
+    @group.setup("session-y")
     def setup_y(session: Session, setup_param: str = "not-set") -> None:
         assert setup_param == "is-set"
         calls.append("setup-y")
 
-    @app.session
+    @group.session
     def session_x_1(session: Session, session_param: bool = False):
         assert session_param
         calls.append("session-x-1")
 
-    @app.session
+    @group.session
     def session_x_2(session: Session, session_param: bool = False):
         assert session_param
         calls.append("session-x-2")
 
-    @app.session
+    @group.session
     def session_y_1(session: Session, session_param: bool = False):
         assert session_param
         calls.append("session-y-1")
 
-    @app.session
+    @group.session
     def session_y_2(session: Session, session_param: bool = False):
         assert session_param
         calls.append("session-y-2")
@@ -257,3 +305,30 @@ def test_setup_funcs(execute: Executor):
         "setup-y",
         "session-y-2",
     ]
+
+
+def test_required_argument(execute: Executor, caplog: pytest.LogCaptureFixture):
+    group = NoxOpt()
+
+    @group.session
+    def session_with_required(session: Session, required: str):
+        assert required == "expected"
+
+    execute(
+        "session-with-required",
+        posargs=[],
+        raises=SystemExit(2),
+        prints=r"the following arguments are required: --required",
+    )
+
+    execute(
+        "session-with-required",
+        posargs=["--required", "wrong"],
+        logs="AssertionError",
+    )
+
+    execute(
+        "session-with-required",
+        posargs=["--required", "expected"],
+        logs="AssertionError",
+    )
